772112208431b96b48b1870481b4fb97a1651aca
agonzalez26 <agonzalez26@student.gsu.edu>
2016-09-29 17:57:49 -0400

    Practice
    Practicing the elements

---
+++ script.js
@@ -0,0 +1,26 @@
+alert("Hello JavaScript alert WORKS!!!");
+
+//getting "Hello World" to change its text
+document.getElementById("p1").innerHTML = "New text!";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+confirm("THis works");
